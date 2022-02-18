8ba0dcb753dfa6a842886950713d007cca29237e
naekang <rlawlsgh6306@gmail.com>
2022-02-18 13:18:51 +0900

    관심사의 분리

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,11 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
     // 구현체를 선택하지 않으면 NullPointerException
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     // join을 호출하면 다형성에 의해 interface가 아닌 구현체인 MemoryMemberRepository안에 있는 save() 호출
     @Override
