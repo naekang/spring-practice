6f88ed27836ca5d6610c755c8f8f6a6ffdb1d3bb
naekang <rlawlsgh6306@gmail.com>
2022-02-27 16:57:38 +0900

    필터

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,10 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan (
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
-
-
 }
